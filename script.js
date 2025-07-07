ef6b3311c25f3a0f54bcbe422b65e55582e1c979
INOjoias <gomesmatheus1712@gmail.com>
2025-07-07 19:25:34 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+let botao = document.querySelector ("#botão");
+let textoSecreto = document.querySelector ("#paragrafoSecreto");
+botao.addEventListener("click", amostre);
+function amostre(){
+    paragrafoSecreto.innerHTML="Tentei fugir de mim, mas aonde eu ia, eu estava! - Autor Desconhecido"
+}
